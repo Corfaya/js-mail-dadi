30177e1b05694e42d6c394f8159deb93c179ef95
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 15:47:13 +0200

    [UPD] h2 and p stored in two variables. added messages on html

--- dice/js/script.js
+++ dice/js/script.js
@@ -6,17 +6,22 @@ play.addEventListener("click", function() {
     // Random number for dice roll
     let randomPlayer = Math.floor(Math.random() * 6 + 1);
     let randomPc = Math.floor(Math.random() * 6 + 1);
-    console.log(randomPlayer, randomPc)
+    // h2#winner and p#victory-info recovery
+    let winner = document.getElementById("winner")
+    let victory = document.getElementById("victory-info")
     // if the winner is player 1...
     if (randomPlayer > randomPc) {
-        console.log(`Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`)
+        winner.innerHTML = "Player 1 vince"
+        victory.innerHTML = `Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`
     }
     // else if there's a draw...
     else if (randomPlayer === randomPc) {
-        console.log(`Pareggio: entrambi hanno totalizzato ${randomPlayer}`)
+        winner.innerHTML = "Pareggio"
+        victory.innerHTML = `Pareggio: entrambi hanno totalizzato ${randomPlayer}`
     }
     // else if the winner is the pc...
     else {
-        console.log(`Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`)
+        winner.innerHTML = "Pc vince"
+        victory.innerHTML = `Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`
     }
 })
