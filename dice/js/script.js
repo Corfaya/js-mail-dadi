380ec47eee810870f4df368f6eb941de316d525a
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 14:57:44 +0200

    [UPD] winner established by showing the result and the proclamation of victory on the console

--- dice/js/script.js
+++ dice/js/script.js
@@ -6,10 +6,17 @@ play.addEventListener("click", function() {
     // Random number for dice roll
     let randomPlayer = Math.floor(Math.random() * 6 + 1);
     let randomPc = Math.floor(Math.random() * 6 + 1);
+    console.log(randomPlayer, randomPc)
     // if the winner is player 1...
-    if (randomPlayer > randomPc) {}
+    if (randomPlayer > randomPc) {
+        console.log(`Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`)
+    }
     // else if there's a draw...
-    else if (randomPlayer === randomPc) {}
+    else if (randomPlayer === randomPc) {
+        console.log(`Pareggio: entrambi hanno totalizzato ${randomPlayer}`)
+    }
     // else if the winner is the pc...
-    else {}
+    else {
+        console.log(`Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`)
+    }
 })
