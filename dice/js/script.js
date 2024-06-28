8fd7003b08874b4af573631fb17e38bf3da84b36
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 16:47:39 +0200

    [FIX] unforgivable mistake fixed. append() replaced with inner.Text

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,37 +1,35 @@
-// Button, #player-one, #pc recovery
+// Button
 const play = document.getElementById("play-game")
-const score = document.getElementById("score");
 
 // click event on 'play'
 play.addEventListener("click", function() {
     // Random number for dice roll
     let randomPlayer = Math.floor(Math.random() * 6 + 1);
     let randomPc = Math.floor(Math.random() * 6 + 1);
-    // h2#winner and p#victory-info recovery
-    let winner = document.getElementById("winner")
-    let victory = document.getElementById("victory-info")
-    //
-    let h4 = document.createElement("h4")
+    // h4 recovery
+    let score = document.getElementById("score")
+    let victory = document.getElementById("victory")
     // if the winner is player 1...
     if (randomPlayer > randomPc) {
         winner.innerHTML = "Player 1 vince"
-        victory.innerHTML = `Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`
-        h4.innerText = `Player 1: ${randomPlayer} - PC: ${randomPc}`
-    score.append(h4);
+        score.innerHTML = `Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`
+        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
+    
     }
     // else if there's a draw...
     else if (randomPlayer === randomPc) {
         winner.innerHTML = "Pareggio"
-        victory.innerHTML = `Pareggio: entrambi hanno totalizzato ${randomPlayer}`
-        h4.innerText = `Player 1: ${randomPlayer} - PC: ${randomPc}`
-    score.append(h4);
+        score.innerHTML = `Pareggio: entrambi hanno totalizzato ${randomPlayer}`
+        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
+    
     }
     // else if the winner is the pc...
     else {
         winner.innerHTML = "Pc vince"
-        victory.innerHTML = `Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`
-        h4.innerText = `Player 1: ${randomPlayer} - PC: ${randomPc}`
-    score.append(h4);
+        score.innerHTML = `Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`
+        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
+    
     }
+    score.append(h4);
     
 })
