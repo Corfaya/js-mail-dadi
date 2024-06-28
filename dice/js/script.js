857127ab3177a029b9a7055c23a34dd20255745a
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 16:29:56 +0200

    [UPD] Style added to dice exercise. DICE CLOSED

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,5 +1,6 @@
-// Button recovery
-let play = document.getElementById("play-game")
+// Button, #player-one, #pc recovery
+const play = document.getElementById("play-game")
+const score = document.getElementById("score");
 
 // click event on 'play'
 play.addEventListener("click", function() {
@@ -9,19 +10,28 @@ play.addEventListener("click", function() {
     // h2#winner and p#victory-info recovery
     let winner = document.getElementById("winner")
     let victory = document.getElementById("victory-info")
+    //
+    let h4 = document.createElement("h4")
     // if the winner is player 1...
     if (randomPlayer > randomPc) {
         winner.innerHTML = "Player 1 vince"
         victory.innerHTML = `Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`
+        h4.innerText = `Player 1: ${randomPlayer}. PC: ${randomPc}`
+    score.append(h4);
     }
     // else if there's a draw...
     else if (randomPlayer === randomPc) {
         winner.innerHTML = "Pareggio"
         victory.innerHTML = `Pareggio: entrambi hanno totalizzato ${randomPlayer}`
+        h4.innerText = `Player 1: ${randomPlayer}. PC: ${randomPc}`
+    score.append(h4);
     }
     // else if the winner is the pc...
     else {
         winner.innerHTML = "Pc vince"
         victory.innerHTML = `Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`
+        h4.innerText = `Player 1: ${randomPlayer}. PC: ${randomPc}`
+    score.append(h4);
     }
+    
 })
