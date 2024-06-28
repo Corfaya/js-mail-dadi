01944373da746d0e1bba0869dcbd27f1f9fc7d80
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 14:52:21 +0200

    [UPD] if statement added for 3 cases: player wins, pc wins, draw

--- dice/js/script.js
+++ dice/js/script.js
@@ -3,4 +3,13 @@ let play = document.getElementById("play-game")
 
 // click event on 'play'
 play.addEventListener("click", function() {
+    // Random number for dice roll
+    let randomPlayer = Math.floor(Math.random() * 6 + 1);
+    let randomPc = Math.floor(Math.random() * 6 + 1);
+    // if the winner is player 1...
+    if (randomPlayer > randomPc) {}
+    // else if there's a draw...
+    else if (randomPlayer === randomPc) {}
+    // else if the winner is the pc...
+    else {}
 })
