781003a26f5ef2cd1ad1a3997cdd6efa8e9f29a3
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 14:07:33 +0200

    [UPD] array created with multiple values. prompt added and stored in a variable

---
+++ mail/js/script.js
@@ -0,0 +1,4 @@
+let emailList = ["pippo_franco@libero.it", "jerry.cala@gmail.com", "sergio-mattarella@pec.governo.it", "emiliofede-100@live.it", "divine-otelma-wizard@hotmail.it", "batman-is-brucewayne@gmail.com", "bilbobaggins@contea.org", "ryu.fighter@street.com", "vittoriofeltri@libero.it", "zeb_eh_volevi.2account@hotmail.it", "snoopdogg70@gmail.com", "onizuka-teacher@anime.jp", "solid-snake@gmail.com", "ww-walterwhite@libero.it", "lauracorrado96@hotmail.it"];
+
+let user_mail = prompt("Inserire l'email").toLowerCase()
+console.log(user_mail)
