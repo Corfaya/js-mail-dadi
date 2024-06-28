4f818309f24e770c6c2fa52a94d7b83e51a32df9
Corfaya <lauracorrado0696@gmail.com>
2024-06-28 14:12:27 +0200

    [UPD] First step: flag variable setted on false. Second step: for statement added with 1 if condition

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,4 +1,14 @@
+// array filled and stored
 let emailList = ["pippo_franco@libero.it", "jerry.cala@gmail.com", "sergio-mattarella@pec.governo.it", "emiliofede-100@live.it", "divine-otelma-wizard@hotmail.it", "batman-is-brucewayne@gmail.com", "bilbobaggins@contea.org", "ryu.fighter@street.com", "vittoriofeltri@libero.it", "zeb_eh_volevi.2account@hotmail.it", "snoopdogg70@gmail.com", "onizuka-teacher@anime.jp", "solid-snake@gmail.com", "ww-walterwhite@libero.it", "lauracorrado96@hotmail.it"];
 
+// prompt stored in "user_mail" variable with toLowerCase method
 let user_mail = prompt("Inserire l'email").toLowerCase()
-console.log(user_mail)
+
+// Flag
+let authorization = false;
+
+for (let i = 0; i < emailList.length; i++) {
+    if (emailList[i] === user_mail) {
+        authorization = true;
+    }
+}
