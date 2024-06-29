2b6d12e912e6cf81007bbad32a8ac47041f04d94
Corfaya <lauracorrado0696@gmail.com>
2024-06-29 17:14:21 +0200

    [UPD] DICE: img added with js's method setAttribute on img tag. img folder created. Improvements CLOSED

--- dice/js/script.js
+++ dice/js/script.js
@@ -9,25 +9,28 @@ play.addEventListener("click", function() {
     // h4 and p recovery
     let score = document.getElementById("score")
     let victory = document.getElementById("victory")
+    // imgs recovery
+    let player = document.getElementById("player-one")
+    let pc = document.getElementById("pc");
+    // [src="img/dice+random"]
+    player.setAttribute("src", `./img/dice${randomPlayer}.png`);
+    pc.setAttribute("src", `./img/dice${randomPc}.png`);
     // if the winner is player 1...
     if (randomPlayer > randomPc) {
         winner.innerHTML = "Player 1 vince"
         score.innerHTML = `Player 1 vince con un ${randomPlayer}, battendo il numero ${randomPc} del PC`
-        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
-    
+        victory.innerHTML = `Player 1: <span class="fw-bolder text-danger">${randomPlayer}</span> PC: <span class="fw-bolder text-danger">${randomPc}</span>`
     }
     // else if there's a draw...
     else if (randomPlayer === randomPc) {
         winner.innerHTML = "Pareggio"
         score.innerHTML = `Pareggio: entrambi hanno totalizzato ${randomPlayer}`
-        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
-    
+        victory.innerHTML = `Player 1: <span class="fw-bolder text-danger">${randomPlayer}</span> PC: <span class="fw-bolder text-danger">${randomPc}</span>`
     }
     // else if the winner is the pc...
     else {
         winner.innerHTML = "Pc vince"
         score.innerHTML = `Il PC vince con un ${randomPc}, battendo il numero ${randomPlayer} del Player 1`
-        victory.innerText = `Player 1: ${randomPlayer} PC: ${randomPc}`
-    
+        victory.innerHTML = `Player 1: <span class="fw-bolder text-danger">${randomPlayer}</span> PC: <span class="fw-bolder text-danger">${randomPc}</span>`
     }    
 })
